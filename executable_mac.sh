7e0a87f1f747250fa4a939f3a6caf157b8351d0c
Luke Shay <shay.luke17@gmail.com>
2022-09-16 15:51:20 -0500

    Cleaned up install

--- executable_mac.sh
+++ executable_mac.sh
@@ -32,21 +32,16 @@ brew install \
   zsh-completions \
   chezmoi
 
-brew install --cask font-fira-code hyper
+brew install --cask font-fira-code
 
-/bin/bash -c "$(curl -fsSL https://spacevim.org/install.sh)"
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
 
 mkdir -p "${HOME}/Developer"
 
-asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
-asdf plugin-add gcloud https://github.com/jthegedus/asdf-gcloud
-asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-asdf plugin-add gradle https://github.com/rfrancis/asdf-gradle.git
 asdf plugin-add java https://github.com/halcyon/asdf-java.git
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin-add python
 asdf plugin-add terraform
-asdf plugin-add v
 
 cd "${PWD}"
